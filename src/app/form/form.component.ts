48ba4b15e17c02002f4de4fd986b70886abc1c99
suraj-Dubey <atuld67861@gmail.com>
2024-12-22 23:52:47 +0530

    form control

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatDialogModule,
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  profileForm = new FormGroup({
+    name : new FormControl(''),
+    age : new FormControl(''),
+    gender : new FormControl(''),
+    phone : new FormControl(''),
+  });
 
-}
+  data : any = []
+  dialog = inject(MatDialog)
+  
+  constructor(){}
+
+  onSubmit(){
+    this.data.push(this.profileForm.value)
+    console.log(this.data)
+  }
+  }
